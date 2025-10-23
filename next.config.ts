12e0c28cdf3005e5d3a5540608b30d47f0b45b6f
YodaFang <yodafang0121@gmail.com>
2025-10-23 19:41:48 +0300

    remove webpack related

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import bundleAnalyzer from '@next/bundle-analyzer';
 
 const nextConfig: NextConfig = {
   images: {
@@ -32,9 +31,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-export default withBundleAnalyzer(nextConfig);
+export default nextConfig;
